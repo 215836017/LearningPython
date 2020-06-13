6a715f5821d9e691f30104303b7d409436b64dfa
215836017 <215836017@qq.com>
2020-06-13 21:33:46 +0800

    modify var.py

--- code/004_input.py
+++ code/004_input.py
@@ -8,6 +8,7 @@ print('Python提供了一个input()，可以让用户从键盘进行输入')
 1. Python提供了一个input()，可以让用户输入字符串，并存放到一个变量里。比如输入用户的名字,
 当你输入name = input()并按下回车后，Python交互式命令行就在等待你的输入了。这时，你可以输入任意字符，然后按回车后完成输入。
 2. input()可以让你显示一个字符串来提示用户
+3. 从input()输入的都是字符串类型
 '''
 print("please input: ")
 name = input()
