0c2cdaa0d30223d949c9c589e9ff04cf96f7ded0
215836017 <215836017@qq.com>
2020-06-27 10:16:28 +0800

    methods of sequence

---
+++ code/014_sequence_splicing.py
@@ -0,0 +1,10 @@
+print('通用的序列操作 -- 序列相加')
+
+letter = ['abc', 'def', 'hij']
+num = [10, 20, 30, 40]
+
+print('num + letter = ', num + letter)
+print('letter + num = ', letter + num)
+# print('num + hello = ', num + 'Hello world') # TypeError: can only concatenate list (not "str") to list
+# 从报错可知：不能拼接列表和字符串
+
